d99dc04e84efe203b75b771138ee20a5be869bca
Jiuglskiy.Vladislav <Jiuglskiy.Vladislav@chtp.net>
2020-05-11 13:53:26 +0300

    upd binary

--- VladBinary.py
+++ VladBinary.py
@@ -1,26 +1,19 @@
-from random import randint
-spis=[123,234,345,456,567,678,789,890,900,1022]
-print(spis)
-key=int(input('Введите число для поиска: '))
-N=right=len(spis)
-left=0
-while True:
-    b=left+((right-left)//2)
-    print('b='+str(b))
-    val=spis[b]
-    print('val='+str(val))
-    print('left='+str(left)+' right='+str(right)+'\n')
-    if (b in [0,(N-1)]):
-        break
-    elif (key==val):
-        break
-    elif (key>val):
-        left=b
+#spis=[-9,-7,0,3,4,9]
+#key=-6
+b = len(spis) // 2
+left = 0
+right = len(spis) - 1
+while spis[b] != key and left <= right:
+    if key > spis[b]:
+        left = b + 1
     else:
-        right=b
-if (key==val):
-    print('Найдено. Индекс: '+str(b))
+        right = b - 1
+    b = (left + right) // 2
+if (b<0):
+    b=0
+if left > right:
+    print("Увы, не найдено. Индекс= ", b) 
 else:
-    print('Увы, такого значения нет')
+    print("Найдено. Индекс=", b)
 
-    
+
